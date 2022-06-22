47bb4b493e5524e17609beef1d32a719a219c18c
aaron <aaronruban60+git@gmail.com>
2022-06-22 15:44:53 -0500

    v1.1

--- timer.py
+++ timer.py
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
 #!/usr/bin/env python3
 from playsound import playsound
 # from os import system, if playsound doesn't work use os.system bash script. EX. afplay
-=======
-
-#!/usr/bin/env python3
-from playsound import playsound
-from os import system
->>>>>>> refs/remotes/origin/main
 from time import sleep
 
 
@@ -24,22 +17,12 @@ def get_timer():
 def timer_set():
     time = get_timer()
     set_period = sleep(time)
-<<<<<<< HEAD
     pa = "/path/to/file/.m4a" #make sure to change this
     playsound(pa)
     print(f'Hi, {time}')
-=======
-    pa = "/Users/aaron/Desktop/funk-loud.m4a"
-    # system(pa)
-    playsound(pa)
-    print(f'Hi, {time}')  # Press ⌘F8 to toggle the breakpoint.
->>>>>>> refs/remotes/origin/main
 
 
 if __name__ == '__main__':
     timer_set()
 
-<<<<<<< HEAD
 #aaron-silicon-valley
-=======
->>>>>>> refs/remotes/origin/main
